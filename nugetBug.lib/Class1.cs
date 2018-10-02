b630828ff62bcdca3fa76939685bb6045c50c701
Georgi Parlakov <Georgi.Parlakov@experian.com>
2018-10-02 10:58:43 +0300

    Add a small code sample

---
+++ nugetBug.lib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace nugetBug.lib
+{
+    public class Class1
+    {
+        public string Method()
+        {
+            return JsonConvert.SerializeObject(new  { t = "test"});
+        }
+    }
+}
